cb995894285bfb6782a059a890f5e405e156332a
oh-mms <oh-mms@gmail.com>
2021-05-12 14:26:27 +0900

    Update solution.cpp

--- 102. Binary Tree Level Order Traversal/solution.cpp
+++ 102. Binary Tree Level Order Traversal/solution.cpp
@@ -1,7 +1,11 @@
+// solution 1
 class Solution {
 public:
 	void process(vector<vector<int>>& nodesForDepth, TreeNode* node, int depth)
 	{
+		if (nodesForDepth.size() - 1 < depth)
+			nodesForDepth.resize(depth + 1);
+
 		nodesForDepth[depth].push_back(node->val);
 
 		if (node->left != nullptr)
@@ -13,15 +17,44 @@ public:
 
 	vector<vector<int>> levelOrder(TreeNode* root)
 	{
-		vector<vector<int>> answer;
-		vector<vector<int>> nodesForDepth(2001);
+		vector<vector<int>> nodesForDepth;
 
 		if (root != nullptr)
+		{
+			nodesForDepth.resize(1);
+			process(nodesForDepth, root, 0);
+		}
+
+		return nodesForDepth;
+	}
+};
+
+/*
+// solution 2
+class Solution {
+public:
+	void process(unordered_map<short, vector<int>>& nodesForDepth, TreeNode* node, int depth)
+	{
+		nodesForDepth[depth].push_back(node->val);
+
+		if(node->left != nullptr)
+			process(nodesForDepth, node->left, depth+1);
+
+		if(node->right != nullptr)
+			process(nodesForDepth, node->right, depth+1);
+	}
+
+	vector<vector<int>> levelOrder(TreeNode* root)
+	{
+		vector<vector<int>> answer;
+		unordered_map<short, vector<int>> nodesForDepth;
+
+		if(root != nullptr)
 			process(nodesForDepth, root, 1);
 
-		for (int i = 1; i < 2001; ++i)
+		for(int i = 1; i < 2001; ++i)
 		{
-			if (nodesForDepth[i].size() <= 0)
+			if(nodesForDepth[i].size() <= 0)
 				break;
 			answer.push_back(nodesForDepth[i]);
 		}
@@ -29,3 +62,4 @@ public:
 		return answer;
 	}
 };
+*/
