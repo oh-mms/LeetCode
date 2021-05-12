acc7acca03c74b46c1bf31dc57359df54ac3b171
oh-mms <oh-mms@gmail.com>
2021-05-12 14:50:24 +0900

    Update solution.cpp

--- 102. Binary Tree Level Order Traversal/solution.cpp
+++ 102. Binary Tree Level Order Traversal/solution.cpp
@@ -3,10 +3,12 @@ class Solution {
 public:
 	void process(vector<vector<int>>& nodesForDepth, TreeNode* node, int depth)
 	{
-		if (nodesForDepth.size() - 1 < depth)
-			nodesForDepth.resize(depth + 1);
+		// 노드의 개수(레벨의 수)가 현재 들어온 노드의 depth(레벨)보다 적으면
+		// 새로 들어온 depth 층의 노드들을 담을 수 있게 백터를 생성(resize)
+		if (nodesForDepth.size() < depth)
+			nodesForDepth.resize(depth);
 
-		nodesForDepth[depth].push_back(node->val);
+		nodesForDepth[depth - 1].push_back(node->val); // 인덱스는 0부터 시작하니
 
 		if (node->left != nullptr)
 			process(nodesForDepth, node->left, depth + 1);
@@ -22,7 +24,7 @@ public:
 		if (root != nullptr)
 		{
 			nodesForDepth.resize(1);
-			process(nodesForDepth, root, 0);
+			process(nodesForDepth, root, 1);
 		}
 
 		return nodesForDepth;
